e630170976ced9d8d385a682928001a8aac6265a
Nikhil-Raghav-004 <nikhilraghavbalivada@gmail.com>
2025-11-01 12:09:32 +0530

    Update Duck_Shooting.py
    Updated Version.2

--- Duck_Shooting.py
+++ Duck_Shooting.py
@@ -1,12 +1,13 @@
 import pygame
-
+import time
 
 pygame.init()
 
 width = 1000
 height = 600
 Score = 0
-
+Life =  3
+Bullet_No = 10
 font = pygame.font.SysFont('Papyrus',50)
 
 screen = pygame.display.set_mode((width, height))
@@ -30,6 +31,9 @@ Ducky1 = pygame.transform.scale(Ducky1,(60,60))
 Ducky2 = pygame.image.load("assets/Objects/duck_white.png")
 Ducky2 = pygame.transform.scale(Ducky2,(30,30))
 
+Life_Less_Duck = pygame.image.load("assets/Objects/duck_brown.png")
+Life_Less_Duck = pygame.transform.scale(Life_Less_Duck,(80,80))
+
 Ducky_Shot = pygame.image.load("assets/Objects/shot_yellow_large.png")
 Ducky_Shot = pygame.transform.scale(Ducky_Shot,(10,10))
 
@@ -39,20 +43,34 @@ Shot_Brown = pygame.transform.scale(Shot_Brown,(20,20))
 Bullet = pygame.image.load("assets/HUD/icon_bullet_gold_short.png")
 Bullet = pygame.transform.scale(Bullet,(10,20))
 
+
+
 Target2 = Ducky2.get_rect()
 Target2.topleft = (100,450)
 speed = 5
 Target = Ducky1.get_rect()
-Target.topleft = (100,360)
+Target.topleft = (100,300)
 Speed = 3
 
+Target_Life = Life_Less_Duck.get_rect()
+Target_Life.topleft = (100,360)
+speeed = 8
+
 Shot_rect = Ducky_Shot.get_rect()
+
+Score_New = pygame.image.load("assets/HUD/text_score_small.png")
+# Score_New = pygame.transform.scale(Score_New)
+
+Colon = pygame.image.load("assets/HUD/text_dots_small.png")
 
 RIFLEE = pygame.image.load("assets/Objects/rifle.png")
 RIFLEE = pygame.transform.scale(RIFLEE,(90,150))
 
 RIFLEE_rect = RIFLEE.get_rect()
 clock = pygame.time.Clock()
+
+Score_NoLis = ["text_0_small.png","text_1_small.png","text_2_small.png","text_3_small.png","text_4_small.png","text_5_small.png","text_6_small.png","text_7_small.png","text_8_small.png","text_9_small.png"]
+
 
 
 Cloud1 = pygame.image.load("assets/Stall/cloud2.png").convert_alpha()
@@ -64,6 +82,39 @@ bullet_list = []
 # global Bullet_rect
 # Bullet_rect = Bullet
 running = True
+
+def Score_Plus(No_Up):
+    Path = "assets/HUD/" + Score_NoLis[No_Up]
+    Img = pygame.image.load(Path)
+    No = pygame.image.load("assets/HUD/text_plus_small.png")
+    screen.blit(No,(290,23))
+    screen.blit(Img,(310,23))
+    # time.sleep(2)
+
+def Display_Life(Life):
+    font = pygame.font.SysFont("Papyrus",50)
+    font.set_bold(True)
+    text = font.render(f'Lives: {Life}',True,'#B8AFC7')
+    screen.blit(text,(820,2))
+
+
+
+def Display_Score(Score):
+    screen.blit(Score_New,(58,25))
+    screen.blit(Colon,(170,30))
+    Digits = str(Score)
+    Space = 190
+    for i in Digits:
+        Path = "assets/HUD/" + Score_NoLis[int(i)]
+        Img = pygame.image.load(Path)
+        screen.blit(Img,(Space,25))
+        Space += 25
+
+def Display_Gameover():
+    Game_Over = pygame.image.load("assets/HUD/text_gameover.png")
+    Game_Over = pygame.transform.scale(Game_Over,(200,100))
+    screen.blit(Game_Over,(400,250))
+
 while running:
     RIFLEE_rect.midbottom = (461,680)
     for event in pygame.event.get():
@@ -78,9 +129,11 @@ while running:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1 :
+                Bullet_No -= 1
                 Bullet_rect = Bullet.get_rect()
                 Bullet_rect.midbottom = RIFLEE_rect.midtop
-                bullet_list.append(Bullet_rect)
+                if len(bullet_list) <= Bullet_No+6:
+                    bullet_list.append(Bullet_rect)
 
 
     screen.blit(Bg,(0,0))
@@ -91,6 +144,9 @@ while running:
     Target2.x += speed
     if Target2.right >= width or Target2.left <= 0:
         speed *= -1
+    Target_Life.x += speeed
+    if Target_Life.right >= width or Target_Life.left <= 0:
+        speeed*= -1
 
 
     b = 40
@@ -112,6 +168,7 @@ while running:
         a = a+256
     screen.blit(Ducky1, Target)
     screen.blit(Ducky2,Target2)
+    screen.blit(Life_Less_Duck,Target_Life)
     for b in bullet_list[:]:
         b.y -= 3
 
@@ -120,13 +177,24 @@ while running:
             Shot_rect.center = Target.center
             screen.blit(Ducky_Shot, Shot_rect)
             Score += 1
+            Score_Plus(1)
 
         if b.colliderect(Target2):
             bullet_list.remove(b)
             Shot_rect.center = Target2.center
             screen.blit(Ducky_Shot, Shot_rect)
             Score += 3
+            Score_Plus(3)
 
+        if b.colliderect(Target_Life):
+            bullet_list.remove(b)
+            Shot_rect.center = Target_Life.center
+            screen.blit(Ducky_Shot, Shot_rect)
+            Life -= 1
+            if Life == 0:
+                Display_Gameover()
+                pygame.time.wait(3000)
+                running = False
 
 
 
@@ -148,8 +216,11 @@ while running:
     screen.blit(Cloud1, (850, 110))
 
 
-    text = font.render(f"Score : {Score}", True,'white')
-    screen.blit(text,(0,0))
+    text = font.render(f"Score : ", True,'white')
+
+    # screen.blit(text,(0,0))
+    Display_Score(Score)
+    Display_Life(Life)
 
     for i in bullet_list:
         screen.blit(Bullet,i)
